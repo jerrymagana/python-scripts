55450b6eac2040a868d54a2924e636e82004c30e
jerrymagana <63806782+jerrymagana@users.noreply.github.com>
2020-04-16 13:15:05 -0400

    Update pullzipcodes.py
    Added comments for clarity.

--- pullzipcodes.py
+++ pullzipcodes.py
@@ -1,3 +1,10 @@
+# ==========================================================================================================================
+# Title: pullzipcodes.py
+# Author: Jerry Paul Magaña
+# Contact: jerrypaulmagana@gmail.com
+# Description: Short script used to clean heterogeneous data in a column of an csv file.
+# ==========================================================================================================================
+
 import pandas as pd
 df = pd.read_csv(r'/home/jpmagana/Documents/Research/Ping Ye Research/2017.addresses.csv')
 addresses = df['ADDRESS']
